cced769397e61f2d37cc78bb3cffb88dc62ca76a
e <1246771571@qq.com>
2018-07-19 20:42:18 +0800

    简单的按钮测试功能

---
+++ app/src/main/java/com/example/administrator/zhangbiao/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.administrator.zhangbiao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "按钮的点击事件", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
